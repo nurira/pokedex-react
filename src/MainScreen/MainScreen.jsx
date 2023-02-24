669a0b91bde81ee3290f5daa8e81a64e03808182
nrira <ranuri@pm.me>
2023-02-24 15:31:24 -0500

    fix styling for main screen

--- src/MainScreen/MainScreen.jsx
+++ src/MainScreen/MainScreen.jsx
@@ -1,9 +1,13 @@
+import Elevate from "../Elevate";
+
 import styles from "./MainScreen.module.scss";
 
 export default function MainScreen({ children }) {
   return (
-    <div className={styles["bezel"]}>
-      <div className={styles["screen"]}>{children}</div>
-    </div>
+    <Elevate color="white">
+      <div className={styles["wrapper"]}>
+        <div className={styles["screen"]}>{children}</div>
+      </div>
+    </Elevate>
   );
 }
