d2aa7c52ae546f1dd35ad3f6d3b17fc289c26785
nrira <ranuri@pm.me>
2023-02-24 19:49:35 -0500

    style main screen implement functionality for turning on/off

--- src/MainScreen/MainScreen.jsx
+++ src/MainScreen/MainScreen.jsx
@@ -1,12 +1,35 @@
 import Elevate from "../Elevate";
 
+import useToggle from "../hooks/useToggle";
+
 import styles from "./MainScreen.module.scss";
 
 export default function MainScreen({ children }) {
+  const [screenIsOn, toggleScreen] = useToggle(false);
+
   return (
     <Elevate color="white">
       <div className={styles["wrapper"]}>
-        <div className={styles["screen"]}>{children}</div>
+        <div className={styles["on-off-button"]} onClick={toggleScreen}></div>
+
+        <div
+          className={styles["screen"]}
+          style={{
+            "--bgColor": screenIsOn ? "hsl(80, 61%, 50%)" : "hsl(0deg 0% 10%)",
+          }}
+        >
+          {screenIsOn && children}
+        </div>
+        <div className={styles["speaker-slots"]}>
+          <div className={styles["slot"]}></div>
+          <div className={styles["slot"]}></div>
+          <div className={styles["slot"]}></div>
+          <div className={styles["slot"]}></div>
+        </div>
+        <div className={styles["top-buttons"]}>
+          <div className={styles["button"]}></div>
+          <div className={styles["button"]}></div>
+        </div>
       </div>
     </Elevate>
   );
